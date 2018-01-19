114248c143b1a65b0056fb87db64f10b664cd9fa
Sebastien Andary <sebastien.andary@gmail.com>
2018-01-19 17:46:58 +0100

    new ex pelvis

--- src/apps/index.js
+++ src/apps/index.js
@@ -3,13 +3,15 @@ import stressApp from './app-exercise-stress';
 import workspaceApp from './app-guide-workspace';
 import postureApp from './app-guide-posture';
 import breathApp from './app-guide-breath';
+import pelvisApp from './app-exercise-pelvis';
 
 const appsTemplates = {
     home: homeApp,
     'app-exercise-stress': stressApp,
     'app-guide-workspace': workspaceApp,
     'app-guide-posture': postureApp,
-    'app-guide-breath': breathApp
+    'app-guide-breath': breathApp,
+    'app-exercise-pelvis': pelvisApp
 };
 
 export default appsTemplates;
