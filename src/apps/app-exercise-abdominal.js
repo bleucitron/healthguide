e6c0ee8830ff220a1f2e7b9e1a8259542fa140df
Sebastien Andary <sebastien.andary@gmail.com>
2018-01-20 21:25:07 +0100

    adds strengthening exercise

---
+++ src/apps/app-exercise-abdominal.js
@@ -0,0 +1,67 @@
+import {
+    showStepTextUntil,
+    setDisplayed,
+    fadeAllStepTexts,
+    showStepText,
+    showStepIconOnly,
+    showStepImageOnly,
+    showElement, hideElement
+} from '../libs/dom-tools';
+import {warmUp, cancelWait, wait} from "../libs/app-helpers";
+import co from 'co';
+
+const stepCount = 3;
+const adviceStepCount = 5;
+const stepTimeout = 5 * 1000; // in ms
+
+function* exercise() {
+    yield* warmUp();
+
+    setDisplayed('.app-page-warm-up', false);
+    setDisplayed('.app-page-1', true);
+    yield wait(stepTimeout);
+
+    for (let step = 1; step < stepCount; step++) {
+        showStepTextUntil(step, stepCount, '.app-page-1 .app-instructions');
+        showStepImageOnly(step, '.app-page-1');
+        showStepIconOnly(step, '.app-page-1');
+        yield wait(stepTimeout);
+    }
+
+    // loop advices
+    while (true) {
+        for (let step = 1; step <= adviceStepCount; step++) { // "<=" because advices start at 1
+            // expiration
+            fadeAllStepTexts('.app-page-1 .app-instructions');
+            showElement(document.querySelector(".app-advices"));
+
+            setDisplayed('.app-advices .step-text', false);
+            setDisplayed(`.app-advices .step-expiration-advice-${step}`, true);
+
+            showStepIconOnly('expiration', '.app-page-1');
+            showStepImageOnly(`expiration-advice-${step}`, '.app-page-1');
+            yield wait(stepTimeout);
+
+            // inspiration
+            hideElement(document.querySelector(".app-advices"));
+
+            showStepIconOnly('inspiration', '.app-page-1');
+            showStepImageOnly('inspiration', '.app-page-1');
+            showStepText('inspiration', '.app-page-1');
+            yield wait(stepTimeout);
+        }
+    }
+}
+
+const app = {
+    title: "Gainage / renforcement musculaire",
+    content: require('../views/app-exercise-abdominal.html'),
+    setup: function () {
+        co(exercise());
+    },
+    exit: function () {
+        cancelWait();
+    }
+};
+
+export default app;
--- src/apps/index.js
+++ src/apps/index.js
@@ -4,6 +4,7 @@ import workspaceApp from './app-guide-workspace';
 import postureApp from './app-guide-posture';
 import breathApp from './app-guide-breath';
 import pelvisApp from './app-exercise-pelvis';
+import abdominalApp from './app-exercise-abdominal';
 
 const appsTemplates = {
     home: homeApp,
@@ -11,7 +12,8 @@ const appsTemplates = {
     'app-guide-workspace': workspaceApp,
     'app-guide-posture': postureApp,
     'app-guide-breath': breathApp,
-    'app-exercise-pelvis': pelvisApp
+    'app-exercise-pelvis': pelvisApp,
+    'app-exercise-abdominal': abdominalApp
 };
 
 export default appsTemplates;
