34b8c26759806d66824309569751311a36f88d2a
Sebastien Andary <sebastien.andary@gmail.com>
2018-01-23 17:36:53 +0100

    adds blobs

--- src/libs/stress-animation.js
+++ src/libs/stress-animation.js
@@ -1,7 +1,13 @@
 const trackPosition = 0.2;
 const trackWidth = 0.15;
+const blobSize = 0.19;
+const blobSpacing = 0.2;
+const blobSizeDecay = 0.6;
+const blobSpacingDecay = 0.7;
+const blobCount = 4;
 
 function wave(t, scale) {return 1 - (Math.cos(t * 2 * Math.PI / scale) * 0.5 + 0.5);}
+function waveGrad(t, scale) {return Math.sin(t * 2 * Math.PI / scale) * Math.PI / scale;}
 
 function createWave(options) {
     const n = 9;
@@ -31,25 +37,27 @@ function createWave(options) {
     return path;
 }
 
-function createBlob(options) {
+function createBlob(options, previous) {
     const amplitude = options.to - options.from;
     const blob = Shape.Circle(new Point(), 1);
-    console.log(blob);
+    
     blob.fillColor = options.color;
     blob.data = {
         update: function(time, period) {
             const scale = view.size.width;
-            const x = options.x * view.size.width;
+            const previousX = previous ? previous.position.x : (0.5 * view.size.width);
+            const gradY = waveGrad(time * scale / period + previousX, scale) * amplitude * view.size.height;
+            const angle = Math.atan2(gradY, 1);
+            const x = previousX - options.spacing * Math.cos(angle) * view.size.width;
             this.radius = options.size * view.size.height;
             this.position.x = x;
             this.position.y = options.from * view.size.height + wave(time * scale / period + x, scale) * amplitude * view.size.height;
-        }.bind(blob)
+        }.bind(blob),
     };
-    blob.data.update(0, 1);
+    //blob.data.update(0, 1);
     // blob.fullySelected = true
     return blob;
 }
-
 
 const paths = [];
 paths.push(createWave({
@@ -64,14 +72,29 @@ paths.push(createWave({
     width: trackWidth,
     color: '#268FAE'
 }));
-paths.push(createBlob({
-    from: trackPosition,
-    to: 1 - trackPosition,
-    size: 0.5*trackWidth + 0.02,
-    color: '#AAE0E3',
-    x: 0.5
-}));
 
+let size = blobSize;
+let spacing = blobSpacing;
+let blob = createBlob({
+        from: trackPosition,
+        to: 1 - trackPosition,
+        size: 0.5 * size,
+        color: '#AAE0E3',
+        spacing: 0
+    });
+paths.push(blob);
+for (let i=1; i < blobCount; i++) {
+    size *= blobSizeDecay;
+    spacing *= blobSpacingDecay;
+    blob = createBlob({
+        from: trackPosition,
+        to: 1 - trackPosition,
+        size: 0.5 * size,
+        color: '#AAE0E3',
+        spacing: spacing
+    }, blob);
+    paths.push(blob);
+}
 
 function onFrame(event) {
     paths.forEach(path => {
