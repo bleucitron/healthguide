e74f1d0bb09bcfffa6c242367d41e4120b4e80cc
Sebastien Andary <sebastien.andary@gmail.com>
2018-02-02 09:05:13 +0100

    rename marche pied to repose pied

--- src/apps/index.js
+++ src/apps/index.js
@@ -1,5 +1,5 @@
 import homeApp from './home';
-import stressApp from './app-exercise-stress';
+//import stressApp from './app-exercise-stress';
 import stressApp2 from './app-exercise-stress-2';
 import workspaceApp from './app-guide-workspace';
 import postureApp from './app-guide-posture';
@@ -9,7 +9,7 @@ import abdominalApp from './app-exercise-abdominal';
 
 const appsTemplates = {
     home: homeApp,
-    'app-exercise-stress': stressApp,
+    //'app-exercise-stress': stressApp,
     'app-exercise-stress-2': stressApp2,
     'app-guide-workspace': workspaceApp,
     'app-guide-posture': postureApp,
